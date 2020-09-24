47cac323c66a3ac8ca45e7a98cbd5d6594d3a131
wangdan <1514952826@qq.com>
2020-09-24 19:07:13 +0800

    完成个人中心页面用户信息和最近播放列表展示

---
+++ pages/login/login.js
@@ -0,0 +1,126 @@
+// pages/login/login.js
+import request from '../../utils/request.js';
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    phone:"",
+    password:""
+  },
+
+  // 向事件回调函数内部传参，需要利用标签属性或者自定义属性
+  handleChange(event){
+    let type = event.currentTarget.id;
+    this.setData({
+      [type]:event.detail.value
+    })
+  },
+
+  handlePassword(event){
+    this.setData({
+      password:event.detail.value
+    })
+  },
+
+// 登录逻辑
+  async handleLogin(){
+    // 1.收集表单数据
+    let {phone,password} = this.data;
+
+    // 2. 前端表单验证
+    let phoneReg = new RegExp(/^1[0-9]{10}/);
+    let pwdReg = new RegExp(/[a-zA-Z0-9]{8}/);
+    // 先验证手机号是否符合规则
+    if(!phoneReg.test(phone)){
+      wx.showToast({
+        title:"手机号码格式不正确",
+        icon:"none"
+      })
+      return;
+    }
+    // 手机号验证通过再执行下一步，验证密码
+    if(!pwdReg.test(password)){
+      wx.showToast({
+        title:"密码格式不正确",
+        icon:"none"
+      })
+      return
+    }
+
+    // 3. 后端表单验证
+    // 发送请求
+    let result = await request("/login/cellphone",{phone,password});
+    console.log(result.profile); //打印用户信息，测试请求结果：密码错误等
+    // 将用户数据信息保存
+    wx.setStorage({
+      key:"userInfo",
+      data:JSON.stringify(result.profile)
+    })
+    wx.showToast({
+      title: '登录成功',
+      icon:"success"
+    })
+    // 登录成功后跳转到个人中心页面
+    wx.switchTab({
+      url: '/pages/personal/personal',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,4 +1,5 @@
 // pages/personal/personal.js
+import request from "../../utils/request.js";
 Page({
 
   /**
@@ -6,7 +7,9 @@ Page({
    */
   data: {
     moveDistance:0,
-    moveTransition:"none"
+    moveTransition:"none",
+    userInfo:{},
+    playList:[]
   },
 
   handleTouchStart(event){
@@ -38,11 +41,31 @@ Page({
     })
   },
 
+  toLogin(){
+    wx.navigateTo({
+      url: '/pages/login/login',
+    })
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad:async function (options) {
+    // 读取 Storage中的数据，更新到data中，进行动态渲染
+    let userInfoStr = wx.getStorageSync("userInfo");
+    //console.log(userInfoStr);
+    if(!this.data.userInfo.nickname && userInfoStr){
+      // 如果 Storage 中有存储用户的数据，就更新到data中
+      this.setData({
+        userInfo:JSON.parse(userInfoStr)
+      })
+    }
+    
+    // 获取用户最近播放记录
+    let playListData = await request("/user/record",{uid:this.data.userInfo.userId,type:1})
+    this.setData({
+      playList:playListData.weekData.splice(0,10)
+    })
   },
 
   /**
